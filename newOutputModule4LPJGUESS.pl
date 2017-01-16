246056971f3cfd1aa543ddf23582b28444e73860
Joerg Steinkamp <joerg.steinkamp@senckenberg.de>
2017-01-16 14:05:51 +0100

    Now including fprint output statements;

--- newOutputModule4LPJGUESS.pl
+++ newOutputModule4LPJGUESS.pl
@@ -12,21 +12,22 @@ my $fname = shift;
 #################################
 ### Define the output culumns ###
 #################################
-sub column_definition($$$) {
-    my $name        = shift;
-    my $columns_ref = shift;
-    my $use_tables  = shift;
 
-    my $column_def = "";
+sub column_output($$$) {
+    my $name       = shift;
+    my $column_ref = shift;
+    my $use_tables = shift;
+
+    my $output_line = "";
     if ($use_tables) {
-        print("Not yet ready!");
+        die ("'use_tables' not yet ready!");
     } else {
-        my $cnames = join ' ', map {
-            $_->{name};
-        } @$columns_ref;
-        $column_def = "        fprintf(out_${name}, 'Lon Lat Year ".$cnames."');\n";
+        my $output_line = join "\n", map {
+            "fprintf(out_${name}, ' %$_->{length}.$_->{dev}$_->{type} ', ". uc("${name}.$_->{name});");
+        } @$column_ref;
+        print($output_line);
     }
-    return($column_def);
+    return($output_line);
 }
 
 ###########################################################
@@ -57,7 +58,7 @@ sub header($$$$) {
 #include "guess.h"
 namespace GuessOutput \{
   REGISTER_OUTPUT_MODULE("${name}", ${ucf_name}Output)
-};  
+};
     } elsif ($suffix eq "h") {
         $header .= qq{#ifndef LPJ_GUESS_${uc_name}_OUTPUT_H
 #define LPJ_GUESS_${uc_name}_OUTPUT_H
@@ -82,19 +83,58 @@ namespace GuessOutput \{
 #####################################################
 ### function returning the tail of the h/cpp file ###
 #####################################################
-sub tail($$) {
+sub tail(@) {
+    my $nargs = @_;
     my $name = shift;
     my $suffix = shift;
+    die ("Not enough arguments ($nargs) for sub 'tail' with 'suffix = cpp'!") if ($suffix eq "cpp" && $nargs < 4);
+    my $ref_files;
+    my $use_tables;
+    if ($suffix eq "cpp") {
+        $ref_files   = shift;
+        $use_tables  = shift;
+    }
     my $ucf_name = ucfirst($name);
     my $tail;
     if ($suffix eq "cpp") {
-        $tail = qq{///* DEFINE DAILY OUTOUT HERE *///
-  void ${ucf_name}Output::outdaily(Gridcell& gridcell) \{
-    return;
+        $tail = qq{  void ${ucf_name}Output::outdaily(Gridcell& gridcell) \{
+/* DEFINE DAILY OUTPUT HERE
+     1.) Create the appropriate stand/patch/vegetation loops around the following fprint statements.
+     2.) Replace the uppercase variable names by the correct variable names of the LPJ-GUESS model
+*/
+};
+        my @files = @$ref_files;
+        while(@files) {
+            my $dom = shift(@files);
+            my $file_name = $dom->findvalue('./name');
+            next if ($dom->findvalue('daily') eq "0" || !$dom->findvalue('daily'));
+            die("JS_DEBUG: daily not yet ready '$file_name'");
+        }
+
+        $tail .= qq{    return;
   \}
-///* DEFINE ANNUAL AND MONTHLY OUTPUT HERE *///
   void ${ucf_name}Output::outannual(Gridcell& gridcell) \{
-    return;
+/* DEFINE ANNUAL AND MONTHLY OUTPUT HERE
+     1.) Create the appropriate stand/patch/vegetation loops around the following fprint statements.
+     2.) Replace the uppercase variable names by the correct variable names of the LPJ-GUESS model
+*/
+};
+        @files = @$ref_files;
+        while(@files) {
+            my $dom = shift(@files);
+            my $file_name = $dom->findvalue('./name');
+            next if ($dom->findvalue('daily') ne "0");
+            my @nodes = $dom->findnodes("./column");
+            while (@nodes) {
+                my $column = shift(@nodes);
+                if ($column->{'type'} eq "s" || $column->{'type'} eq "i") {
+                    $tail .="    fprintf(out_${file_name}, ' %$column->{length}$column->{type}', ". uc("${file_name}.$column->{name});\n");
+                } else {
+                    $tail .= "    fprintf(out_${file_name}, ' %$column->{length}.$column->{dec}$column->{type}', ". uc("${file_name}.$column->{name});\n");
+                }
+            }
+        }
+        $tail .= qq{    return;
   \}
 \}
 };
@@ -104,9 +144,9 @@ sub tail($$) {
 #endif
 };
     } else {
-        return(0);
+        die("File suffix '$suffix' unknown!");
     }
-    return($tail);     
+    return($tail);
 }
 #############################################################
 ### function to declare the output variable in the header ###
@@ -115,35 +155,36 @@ sub declare($$) {
     my $ref_files = shift;
     my $use_tables = shift;
     my @files = @$ref_files;
-    my $dec = "";
+    my $declaration = "";
     while (@files) {
-        my $v = shift(@files);
-        $dec .= "    xtring file_$v;\n";
+        my $dom = shift(@files);
+        my $file_name = $dom->findvalue('./name');
+        $declaration .= "    xtring file_${file_name};\n";
         if ($use_tables) {
-            $dec .= "    Table *out_$v;\n";
+            $declaration .= "    Table *out_${file_name};\n";
         } else {
-            $dec .= "    FILE *out_$v;\n";
+            $declaration .= "    FILE *out_${file_name};\n";
         }
     }
-    return($dec);
+    return($declaration);
 }
 ###############################################
 ### function to declare ins-file parameters ###
 ###############################################
-sub declare_ins($$$) {
-    my $name                  = shift;
-    my $ref_files             = shift;
-    my $ref_files_description = shift;
-    my $ucf_name = ucfirst($name);
+sub declare_ins($$) {
+    my $name      = shift;
+    my $ref_files = shift;
+    my $ucf_name  = ucfirst($name);
     my @files = @$ref_files;
-    my @files_description = @$ref_files_description;
-    my $nfiles = @files;
     my $dec = "  ${ucf_name}Output::${ucf_name}Output() {\n";
-    for (my $i=0; $i < $nfiles; $i++) {
-        $dec .= "    declare('file_$files[$i]', &file_$files[$i], 300, '$files_description[$i]')\n";
+    while(@files) {
+        my $dom = shift(@files);
+        my $file_name = $dom->findvalue('./name');
+        my $file_description = $dom->findvalue('./description');
+        $dec .= "    declare('file_$file_name', &file_$file_name, 300, '$file_description');\n";
     }
     $dec .= qq{  \}
-  
+
   ProfoundOutput::~ProfoundOutput() \{
   \}
 };
@@ -152,12 +193,11 @@ sub declare_ins($$$) {
 #########################################
 ### function to initialize the output ###
 #########################################
-sub init_output($$$$) {
-    my $name        = shift;
-    my $ref_files   = shift;
-    my $ref_columns = shift;
-    my $use_tables  = shift;
-    
+sub init_output($$$) {
+    my $name       = shift;
+    my $ref_files  = shift;
+    my $use_tables = shift;
+
     my $ucf_name = ucfirst($name);
     my @files = @$ref_files;
     my $init = "  void ${ucf_name}Output::init() {\n";
@@ -190,26 +230,30 @@ sub init_output($$$$) {
       month_columns_wide += ColumnDescriptor(months[i], 10, 3);
     \}
 };
-        
+
         $init .= "  }\n";
-
-
     } else {
         while (@files) {
-            my $fname = shift(@files);
-            $init .= qq{    if (file_${fname} != "") \{
-      std::string full_path = (char*) file_${fname};
-      out_${fname} = fopen(full_path.c_str(), "w");
-      if (out_${fname} == NULL) \{
+            my $file = shift(@files);
+            my $file_name = $file->findnodes('./name');
+            $init .= qq{    if (file_${file_name} != "") \{
+      std::string full_path = (char*) file_${file_name};
+      out_${file_name} = fopen(full_path.c_str(), "w");
+      if (out_${file_name} == NULL) \{
         fail("Could not open %s for output\\n"\\
              "Close the file if it is open in another application",
              full_path.c_str());
       \} else \{
 };
 
-            $init .= column_definition($fname, @${ref_columns{$fname}}, $use_tables);
-#        fprintf(out_${fname}, "Lon Lat Year ENTER THE COLUMN DEFINITIONS HERE\n");
-
+            if ($use_tables) {
+                die ("JS_DEBUG: Table definition not yet ready!");
+            } else {
+                my $cnames = join ' ', map {
+                    $_->{name};
+                } $file->findnodes('./column');
+                $init .= "        fprintf(out_${file_name}, 'Lon Lat Year ".$cnames."');\n";
+            }
             $init .= qq{      \}
     \}
 };
@@ -229,17 +273,18 @@ my $use_tables = $dom->findnodes('/GuessOutput/use_tables')->to_literal();
 my $author     = $dom->findnodes('/GuessOutput/author')->to_literal();
 
 my @files = ();
-my @files_description = ();
-my %columns = ();
+my @file_names = ();
+my @file_descriptions = ();
 
+### output files
 foreach my $file ($dom->findnodes('/GuessOutput/file')) {
-    my $foutname = $file->findvalue('./name');
-    push(@files, $foutname);
-    push(@files_description, $file->findvalue('./description'));
-    
-    my @columns = $file->findnodes('./column');
-    $columns{$foutname} = \@columns;
+    push(@files, $file);
+    my $file_name = $file->findvalue('./name');
+    die("Empty file names not allowed!\n${file}\n") if ($file_name eq "");
+    push(@file_names, $file_name);
+    push(@file_descriptions, $file->findvalue('./description'));
 }
+
 #######################################################
 ### create the output files in the current work dir ###
 #######################################################
@@ -251,7 +296,7 @@ close HEADEROUT;
 
 open(CPPOUT, "> ${name}output.cpp");
 print CPPOUT header($name, $long_name, $author, "cpp");
-print CPPOUT declare_ins($name, \@files, \@files_description);
-print CPPOUT init_output($name, \@files, \%columns, $use_tables);
-print CPPOUT tail($name, "cpp");
+print CPPOUT declare_ins($name, \@files);
+print CPPOUT init_output($name, \@files, $use_tables);
+print CPPOUT tail($name, "cpp", \@files, $use_tables);
 close CPPOUT;
